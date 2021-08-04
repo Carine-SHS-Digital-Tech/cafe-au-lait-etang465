0abbb82bef44d6b705bec6b7bdda0ffa94ab670e
etang465 <86392096+etang465@users.noreply.github.com>
2021-08-04 11:44:32 +0800

    STARTED FINAL CODE.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+print('Café au Lait daily summary. \n')
+
+menu = {1: 'cuppaccino $3.00', 2: 'esppresso $2.25', 3: 'latte $2.50', 4: 'iced coffee $2.50'}
+inorout = input('dine in or take away? \n')
+
+print(menu)
+order = input('\nwhich coffee would you like to order?')
+
+
+
+
+
+
+
+
+
+
