8ab2809a75221c1bd39f752a5de190c8781f9072
etang465 <86392096+etang465@users.noreply.github.com>
2021-08-05 21:15:24 +0800

    added the surcharge.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#ORDER DETAILS
+
 print('Café au Lait daily summary. \n')
 menu = {'cappuccino': 3.00,'espresso': 2.25,'latte': 2.50,'iced coffee': 2.50}
 inorout = input('dine in or take away? \n')
@@ -15,9 +17,17 @@ while more == 'yes':
         quan = int(input('how many iced coffees?\n'))
     more = input('would you like to order more?\n')
 GST = float(menu[order]*quan*0.1)
-print(f'your order/recipt: {quan} {order}s, $ {menu[order]*quan}, {inorout}, GST: {GST}, TOTAL AMOUNT: $ {GST + menu[order]*quan}.')
+total1 = GST + menu[order]*quan
+if inorout == 'take away':
+    surcharge = '5%'
+    total = total1/100*5 + total1
+elif inorout == 'dine in':
+    surcharge = '0%'
+    total = GST + menu[order]*quan
+format_total = '{:.2f}'.format(total)
+print(f'your order/recipt: {quan} {order}/s, ${menu[order]*quan}, {inorout}, surcharge: {surcharge}, $GST: {GST}, TOTAL AMOUNT: ${format_total}.')
 
-DAILY SUMMARY
+#DAILY SUMMARY
 
 
 
