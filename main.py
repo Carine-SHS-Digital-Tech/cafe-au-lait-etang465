b6bd6a69f5428184e708cb5b2cd19d3fad366625
etang465 <86392096+etang465@users.noreply.github.com>
2021-08-05 11:02:56 +0800

    ayo im so good. (i get by with a lil help from my friends).

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 print('Café au Lait daily summary. \n')
-menu = {1: 'cuppaccino $3.00', 2: 'esppresso $2.25', 3: 'latte $2.50', 4: 'iced coffee $2.50'}
+menu = {'cappuccino': 3.00,'espresso': 2.25,'latte': 2.50,'iced coffee': 2.50}
 inorout = input('dine in or take away? \n')
 print(menu)
-order = input('\nwhich coffee would you like to order?')
+more = 'yes'
 while more == 'yes':
-    input(order)
-    if order == 'cuppaccino' or order == '1':
-        input('how many cuppaccinos?')
-    elif order == 'esspresso' or order == '2':
-        input('how many esppressos?')
-    elif order == 'latte' or order == '3':
-        input('how many lattes?')
-    elif order == 'iced coffee' or order == '4':
-        input('how many iced coffees?')
-    more = input('whould you like to order more?')
+    order = input('\nwhich coffee would you like to order?\n')
+    if order == 'cappuccino':
+        quan = int(input('how many cappuccinos?\n'))
+    elif order == 'espresso':
+        quan = int(input('how many espressos?\n'))
+    elif order == 'latte':
+        quan = int(input('how many lattes?\n'))
+    elif order == 'iced coffee':
+        quan = int(input('how many iced coffees?\n'))
+    more = input('would you like to order more?\n')
+GST = float(menu[order]*quan*0.1)
+print(f'your order/recipt: {quan} {order}s, $ {menu[order]*quan}, {inorout}, GST: {GST}, TOTAL AMOUNT: $ {GST + menu[order]*quan}.')
 
-
-
-
+DAILY SUMMARY
 
 
 
